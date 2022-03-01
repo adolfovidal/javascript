4e2f2f9e8e935fba8ce3725106021bb2b931239c
adolfovidal <adolfosvidal@gmail.com>
2022-03-01 18:14:16 -0300

    Update ambiente.js

--- exercicios/aula13/ambiente.js
+++ exercicios/aula13/ambiente.js
@@ -18,9 +18,22 @@ console.log('tudo bem')
 
 // Estrutura de repetição com teste lógico no final
 
+/*
 var  c = 1
 do { // faça
     console.log(`Passo ${c}`)
     c++ // c = c + 1
 } while (c <= 6)
 
+*/
+
+var c = 1
+while (c <=10) {
+    
+    c++
+}
+
+for (c = 1 ; c <= 10  ;  C++ ) {
+
+
+}
