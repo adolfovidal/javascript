ac079a1697a02c54a32571b70a1583fef134ca17
adolfovidal <adolfosvidal@gmail.com>
2022-03-01 17:59:49 -0300

    aula13 rep pt1

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,26 @@
+// Estrutura de repetição com teste lógico no inicio
+
+/*
+
+var  c = 1
+while(c <= 99) { // enquanto
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+*/
+
+// Estrutura de repetição com teste lógico no final
+
+var  c = 1
+do { // faça
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 6)
+
