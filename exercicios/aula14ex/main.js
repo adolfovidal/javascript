ca00cfe17afd50eeb2330b3fc2009434dff84c89
adolfovidal <adolfosvidal@gmail.com>
2022-03-02 07:42:32 -0300

    desafio2

---
+++ exercicios/aula14ex/main.js
@@ -0,0 +1,11 @@
+function contar() {
+    var startText = document.querySelector('input#stext')
+    var endText = document.querySelector('input#etext')
+    var passText = document.querySelector('input#ptext')
+    var accountResult = document.querySelector('div#res')
+    var sumOfNumbers = Number(startText.value)
+
+    accountResult.innerHTML = ``   
+}
+
+while( sumOfNumbers == )
