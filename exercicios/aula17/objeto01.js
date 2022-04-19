79bea37b321c2a114c1fbdee06b69ae8ad3eb557
adolfovidal <adolfosvidal@gmail.com>
2022-04-19 18:46:47 -0300

    update

--- exercicios/aula17/objeto01.js
+++ exercicios/aula17/objeto01.js
@@ -1,10 +1,13 @@
-let amigo = {nome: 'Paula', 
-sexo: 'F', 
-peso: 54,
-engordar(p=0){
-    console.log('Engordou')
-    this.peso += p
-}}
+let amigo = {
+    nome: "Paula",
+    sexo: "F",
+    peso: 54,
 
-amigo.engordar(2)
-console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
+    engordar(p = 0) {
+        // console.log("Engordou");
+        this.peso += p;
+    },
+};
+
+amigo.engordar(25);
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg`);
